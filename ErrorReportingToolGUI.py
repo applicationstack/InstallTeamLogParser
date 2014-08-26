53d1a76db876d30064149e8f5e722d2c8c13a236
Risa Newyear-Ramirez <risa.newyear-ramirez@sap.com>
2014-08-26 16:33:49 -0700

    Changed unknownErrors to listbox, fixed gui stuff

--- ErrorReportingToolGUI.py
+++ ErrorReportingToolGUI.py
@@ -13,30 +13,32 @@ class ErrorReporting(wx.Frame):
 	def ErrorReportingGUI(self) :
 		ErrorReportingPanel = wx.Panel(self)
 	
-		KnownErrorsList = open('C:\Users\I841251\Desktop\logparser\KnownErrors.txt', 'r')
-		UnknownErrorsList = open('C:\Users\I841251\Desktop\logparser\UnknownErrors.txt', 'r')
+		KnownErrorsList = open('KnownErrors.txt', 'r')
+		UnknownErrorsList = open('UnknownErrors.txt', 'r')
 		KnownErrorsLines = KnownErrorsList.readlines()
 		UnknownErrorsLines = UnknownErrorsList.readlines()
 	
 		# Set up the font, sizing, and boxes
 		font = wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.BOLD)
-		heading = wx.StaticText(self, label='Welcome to the Error Reporting Tool', pos=(140, 15))
+		heading = wx.StaticText(self, label='Welcome to the Error Reporting Tool', pos=(350, 15))
 		heading.SetFont(font)
 		self.SetTitle( 'Error Reporting Tool')
-		self.KnownErrorsTxtCtrl = wx.TextCtrl(self, value=str(KnownErrorsLines), pos=(30, 90), size=(150,150), style=wx.TE_MULTILINE)
-		self.UnknownErrorsListBox = wx.TextCtrl(self, value=str(UnknownErrorsLines), pos=(270, 90), size=(150,150), style=wx.TE_MULTILINE)
+		self.KnownErrorsTxtCtrl = wx.TextCtrl(self, value=str(KnownErrorsLines), pos=(30, 90), size=(300,190), style=wx.TE_MULTILINE)
+		#UnknownErrors as a list box, for selecting errors to report to JIRA
+		self.UnknownErrorsListBox = wx.ListBox(self, -1, (360,90), (650,190), UnknownErrorsLines, wx.LB_SINGLE)
 		self.Bind(wx.EVT_LISTBOX, self.OnUnknownErrorsListBox)
 		
 		#Line for decoration and labels for boxes
-		wx.StaticLine(self, pos=(25, 50), size=(300,1))
-		KnownErrorsText = wx.StaticText(self, label='Known Errors', pos=(30, 60))
+		
+		wx.StaticLine(self, pos=(25, 50), size=(980,5))
+		KnownErrorsText = wx.StaticText(self, label='Known Errors', pos=(120, 60))
 		KnownErrorsText.SetForegroundColour(wx.Colour(34,139,34))
-		UnknownErrorsText = wx.StaticText(self, label='Unknown Errors', pos=(270, 60))
+		UnknownErrorsText = wx.StaticText(self, label='Unknown Errors', pos=(500, 60))
 		UnknownErrorsText.SetForegroundColour(wx.Colour(255,0,0))
 		wx.StaticLine(self, pos=(25, 260), size=(300,1))
 	
 		# When there are unknown errors present, report them to JIRA
-		self.button =wx.Button(self, label="Report to JIRA", pos=(200, 325))
+		self.button =wx.Button(self, label="Report to JIRA", pos=(350, 325))
 		self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
 
 		self.Centre()
